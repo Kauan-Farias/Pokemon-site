1237636e0d58f69678f8280aa3bf1f90da811367
Kauan Farias <Kauan.farias.2505@gmail.com>
2023-08-24 20:41:26 -0300

    5.5

---
+++ js/pokemon-slider.js
@@ -0,0 +1,17 @@
+let count = 1;
+document.getElementById('radio1').checked = true;
+
+setInterval(function(){
+    nextIten()
+}, 120000)
+
+function nextIten(){
+    count++;
+    if(count>4){
+        count = 1;
+    }
+
+    document.getElementById(`radio${count}`).checked = true;
+
+}
+
